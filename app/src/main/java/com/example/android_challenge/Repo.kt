56e9c9ccfda7306b2dc42eec811d6208ea37186a
foram-patel-0201 <foramjpatel0201@gmail.com>
2022-05-06 14:58:03 +0530

    Internet status check

--- app/src/main/java/com/example/android_challenge/Repo.kt
+++ app/src/main/java/com/example/android_challenge/Repo.kt
@@ -1,9 +1,15 @@
 package com.example.android_challenge
 
+import android.content.Context
 import android.content.Intent
+import android.net.ConnectivityManager
+import android.net.NetworkCapabilities
+import android.os.Build
 import android.os.Bundle
+import android.util.Log
 import android.widget.TextView
 import android.widget.Toast
+import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
 import com.android.volley.Request
 import com.android.volley.toolbox.JsonArrayRequest
@@ -28,6 +34,7 @@ class Repo : AppCompatActivity() {
     }
 
 
+    @RequiresApi(Build.VERSION_CODES.M)
     private fun jsonParse() {
 
         val intent = getIntent()
@@ -54,12 +61,40 @@ class Repo : AppCompatActivity() {
             }
         }, { error ->
             error.printStackTrace()
-            var errorCode = error.networkResponse.statusCode.toString()
-            if (errorCode.equals("404")) {
-                startActivity(Intent(this, UserNotFount::class.java))
+            if (isOnline(this)) {
+                var errorCode = error.networkResponse.statusCode.toString()
+                if (errorCode.equals("404")) {
+                    startActivity(Intent(this, UserNotFound::class.java))
+                }
+            } else{
+                Toast.makeText(applicationContext,"Please Turn on The internet and try again."
+                ,Toast.LENGTH_LONG).show()
             }
         })
         val requestQueue = Volley.newRequestQueue(this)
         requestQueue?.add(request)
     }
+}
+
+@RequiresApi(Build.VERSION_CODES.M)
+fun isOnline(context: Context): Boolean {
+    val connectivityManager =
+        context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+    if (connectivityManager != null) {
+        val capabilities =
+            connectivityManager.getNetworkCapabilities(connectivityManager.activeNetwork)
+        if (capabilities != null) {
+            if (capabilities.hasTransport(NetworkCapabilities.TRANSPORT_CELLULAR)) {
+                Log.i("Internet", "NetworkCapabilities.TRANSPORT_CELLULAR")
+                return true
+            } else if (capabilities.hasTransport(NetworkCapabilities.TRANSPORT_WIFI)) {
+                Log.i("Internet", "NetworkCapabilities.TRANSPORT_WIFI")
+                return true
+            } else if (capabilities.hasTransport(NetworkCapabilities.TRANSPORT_ETHERNET)) {
+                Log.i("Internet", "NetworkCapabilities.TRANSPORT_ETHERNET")
+                return true
+            }
+        }
+    }
+    return false
 }
--- app/src/main/java/com/example/android_challenge/UserNotFound.kt
+++ app/src/main/java/com/example/android_challenge/UserNotFound.kt
@@ -3,9 +3,9 @@ package com.example.android_challenge
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class UserNotFount : AppCompatActivity() {
+class UserNotFound : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_user_not_fount)
+        setContentView(R.layout.activity_user_not_found)
     }
 }
