006f052f7b8b535b02927cd968cf86005966d9c5
foram-patel-0201 <foramjpatel0201@gmail.com>
2022-05-06 13:51:54 +0530

    get repo names

--- app/src/main/java/com/example/android_challenge/Repo.kt
+++ app/src/main/java/com/example/android_challenge/Repo.kt
@@ -1,11 +1,51 @@
 package com.example.android_challenge
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.android.volley.Request
+import com.android.volley.toolbox.JsonArrayRequest
+import com.android.volley.toolbox.Volley
+import org.json.JSONException
+import org.json.JSONObject
+
 
 class Repo : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_repo)
+
+
+        jsonParse()
+    }
+
+
+    private fun jsonParse() {
+
+        val intent = getIntent()
+        val Username = intent.extras!!.getString("username")
+
+        var textView: TextView = findViewById(R.id.textView_repo)
+
+        val url = "https://api.github.com/users/" + Username + "/repos"
+        val request = JsonArrayRequest(Request.Method.GET, url, null, { response ->
+            try {
+
+                for (i in 0 until response.length()) {
+                    val repoObject: JSONObject = response.getJSONObject(i)
+
+                    val repoName =
+                        repoObject.getString("name")
+
+
+                    textView.append("$repoName,\n")
+                }
+            } catch (e: JSONException) {
+                e.printStackTrace()
+            }
+        }, { error -> error.printStackTrace() })
+        val requestQueue = Volley.newRequestQueue(this)
+        requestQueue?.add(request)
     }
 }
--- app/src/main/java/com/example/android_challenge/Username.kt
+++ app/src/main/java/com/example/android_challenge/Username.kt
@@ -1,11 +1,30 @@
 package com.example.android_challenge
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.view.View
+import android.widget.TextView
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+
 
 class Username : AppCompatActivity() {
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_username)
+
+
+    }
+
+    fun getRepo(view: View) {
+
+
+        var username: TextView = findViewById(R.id.username)
+        val Username = username.getText().toString()
+        val i = Intent(this, Repo::class.java)
+        i.putExtra("username", Username)
+        startActivity(i)
     }
 }
