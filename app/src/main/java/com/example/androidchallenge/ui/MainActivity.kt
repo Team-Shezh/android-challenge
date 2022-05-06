eb274e13f34f7154953b698f0ceeee2ca42606a3
SherlockVARM <anmoldhiman7111@gmail.com>
2022-05-07 04:21:43 +0530

    ui for the main activity i.e. user input activity is done

--- app/src/main/java/com/example/androidchallenge/ui/MainActivity.kt
+++ app/src/main/java/com/example/androidchallenge/ui/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.example.androidchallenge
+package com.example.androidchallenge.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.androidchallenge.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
