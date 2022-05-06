cb5131aa72cfa6218899734f8b3acfbc2b93d22a
SherlockVARM <anmoldhiman7111@gmail.com>
2022-05-07 04:29:52 +0530

    intent after checking user input is set

---
+++ app/src/main/java/com/example/androidchallenge/RepositoryList.kt
@@ -0,0 +1,11 @@
+package com.example.androidchallenge
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class RepositoryList : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_repository_list)
+    }
+}
--- app/src/main/java/com/example/androidchallenge/ui/MainActivity.kt
+++ app/src/main/java/com/example/androidchallenge/ui/MainActivity.kt
@@ -1,12 +1,31 @@
 package com.example.androidchallenge.ui
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.example.androidchallenge.R
+import android.util.Log
+import android.widget.Toast
+import com.example.androidchallenge.RepositoryList
+import com.example.androidchallenge.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
+        binding = ActivityMainBinding.inflate(layoutInflater)
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
+
+        binding.findButton.setOnClickListener {
+            Log.d("main", binding.userName.text.toString().trim().replace(' ', '-').lowercase())
+            if (binding.userName.text.toString().trim() != "") {
+                val intent = Intent(this, RepositoryList::class.java)
+                intent.putExtra("userName", binding.userName.text.toString().trim().replace(' ', '-').lowercase())
+                startActivity(intent)
+                finish()
+            } else  {
+                Toast.makeText(this, "Enter the user name", Toast.LENGTH_SHORT).show()
+            }
+
+        }
     }
 }
